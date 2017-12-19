0fd5151813de3678f1a66bc20b176336c0f7adee
toolzpro <toolzz@tuta.io>
2017-12-19 13:34:19 +0300

    split img

--- assets/pager.js
+++ assets/pager.js
@@ -1,4 +1,4 @@
-/* ! Webflow: Front-end site library @license MIT Inline scripts may access the api using an async handler:   var Webflow = Webflow || [];   Webflow.push(readyFunction); */
+/* pager */
 !function(t) {
   function e(r) {
     if (n[r])
